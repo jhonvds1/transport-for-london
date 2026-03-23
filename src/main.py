0ac4d720eabdc07c37fb4b0f88867ad8f896e1bd
jonatha silva <jonatha.sviegas@hotmail.com>
2026-03-23 15:59:11 -0300

    pipeline ok

--- src/main.py
+++ src/main.py
@@ -2,13 +2,15 @@ from src.extract.extract import run_extract
 from src.transform.transform import run_transform
 from src.load.load import run_load
 import logging
+from pyspark.sql import SparkSession
 
 
 def run_pipeline():
 
+    spark = SparkSession.builder.appName("spark").getOrCreate()
     run_extract()
-    data = run_transform()
-    run_load(data)
+    data = run_transform(spark)
+    run_load(data, spark)
 
 
 if __name__ == "__main__":
