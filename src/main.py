0ea50bd8820ff69fcd9e6c5bd659911cd4240bb6
jonatha silva <jonatha.sviegas@hotmail.com>
2026-04-04 15:11:00 -0300

    main glue finalizado

--- src/main.py
+++ src/main.py
@@ -1,22 +1,50 @@
+import sys
+import logging
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
 from src.extract.extract import run_extract
 from src.transform.transform import run_transform
 from src.load.load_s3 import run_load
-from pyspark.sql import SparkSession
 
+# 🔧 configuração de log
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
-def run_pipeline():
+# args do Glue
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
-    spark = SparkSession.builder \
-    .config(
-        "spark.hadoop.fs.s3a.aws.credentials.provider",
-        "com.amazonaws.auth.DefaultAWSCredentialsProviderChain"
-    ) \
-    .getOrCreate()
+logger.info("Inicializando contexto do Glue")
 
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+try:
+    logger.info("Iniciando etapa de EXTRAÇÃO")
     run_extract()
+    logger.info("Extração finalizada com sucesso")
+
+    logger.info("Iniciando etapa de TRANSFORMAÇÃO")
     data = run_transform(spark)
+    logger.info("Transformação finalizada com sucesso")
+
+    logger.info("Iniciando etapa de LOAD")
     run_load(data, spark)
+    logger.info("Carga finalizada com sucesso")
 
+    logger.info("Finalizando job")
+    job.commit()
 
-if __name__ == "__main__":
-    run_pipeline()
+except Exception as e:
+    logger.error(f"Erro no pipeline: {str(e)}", exc_info=True)
+    raise
